8356382487f322cab9ec9c9d14d7fbd566c567b3
杨伟锋 <paranoidyang@163.com>
2019-03-07 16:59:41 +0800

    添加博客分类的修改和删除

--- routers/admin.js
+++ routers/admin.js
@@ -49,7 +49,7 @@ router.get('/user', function(req, res, next) {
 
   User.count().then(function(count) {
 
-    //计算总页数
+    //计算总页数(向上取整)
     pages = Math.ceil(count / limit);
     //取值不能超过pages
     page = Math.min( page, pages );
@@ -78,14 +78,42 @@ router.get('/user', function(req, res, next) {
  * 分类首页
  */
 router.get('/category', function (req, res, next) {
-  res.render('admin/category_index', {
-    userInfo: req.userInfo
-  })
+  var page = Number(req.query.page || 1);
+  var limit = 10;
+  var pages = 0;
 
+  Category.count().then(function(count) {
+
+    //计算总页数
+    pages = Math.ceil(count / limit);
+    //取值不能超过pages
+    page = Math.min( page, pages );
+    //取值不能小于1
+    page = Math.max( page, 1 );
+
+    var skip = (page - 1) * limit;
+
+    /*
+    * 1: 升序
+    * -1: 降序
+    * */
+    Category.find().sort({_id: -1}).limit(limit).skip(skip).then(function(categories) {
+      res.render('admin/category_index', {
+        userInfo: req.userInfo,
+        categories: categories,
+
+        count: count,
+        pages: pages,
+        limit: limit,
+        page: page
+      });
+    });
+
+  });
 })
 
 /**
- * 分类首页
+ * 添加分类
  */
 router.get('/category/add', function (req, res, next) {
   res.render('admin/category_add', {
@@ -137,5 +165,114 @@ router.post('/category/add', function (req, res, next) {
   })
 
 })
+
+/**
+ * 修改分类
+ */
+router.get('/category/edit', function(req, res) {
+
+  //获取要修改的分类的信息，并且用表单的形式展现出来
+  var id = req.query.id || '';
+
+  //获取要修改的分类信息
+  Category.findOne({
+    _id: id
+  }).then(function(category) {
+    if (!category) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '分类信息不存在'
+      });
+    } else {
+      res.render('admin/category_edit', {
+        userInfo: req.userInfo,
+        category: category
+      });
+    }
+  })
+
+})
+
+/**
+ * 保存修改分类
+ */
+router.post('/category/edit', function(req, res) {
+
+  //获取要修改的分类的信息，并且用表单的形式展现出来
+  var id = req.query.id || '';
+  //获取post提交过来的名称
+  var name = req.body.name || '';
+
+  //获取要修改的分类信息
+  Category.findOne({
+    _id: id
+  }).then(function(category) {
+    if (!category) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '分类信息不存在'
+      });
+      return Promise.reject();
+    } else {
+      //当用户没有做任何的修改提交的时候
+      if (name == category.name) {
+        res.render('admin/success', {
+          userInfo: req.userInfo,
+          message: '修改成功',
+          url: '/admin/category'
+        });
+        return Promise.reject();
+      } else {
+        //要修改的分类名称是否已经在数据库中存在
+        return Category.findOne({
+          _id: {$ne: id},
+          name: name
+        });
+      }
+    }
+  }).then(function(sameCategory) {
+    if (sameCategory) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '数据库中已经存在同名分类'
+      });
+      return Promise.reject();
+    } else {
+      return Category.update({
+        _id: id
+      }, {
+        name: name
+      });
+    }
+  }).then(function() {
+    res.render('admin/success', {
+      userInfo: req.userInfo,
+      message: '修改成功',
+      url: '/admin/category'
+    });
+  })
+
+});
+
+/**
+ * 删除分类
+ */
+router.get('/category/delete', function(req, res) {
+
+  //获取要删除的分类的id
+  var id = req.query.id || '';
+
+  Category.remove({
+    _id: id
+  }).then(function() {
+    res.render('admin/success', {
+      userInfo: req.userInfo,
+      message: '删除成功',
+      url: '/admin/category'
+    });
+  });
+
+})
+
 
 module.exports = router
