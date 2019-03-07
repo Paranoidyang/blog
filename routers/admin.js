2d92ece0536e21cced2de74baa868037fdd65af5
杨伟锋 <paranoidyang@163.com>
2019-03-07 21:05:12 +0800

    博客内容的添加、编辑和删除

--- routers/admin.js
+++ routers/admin.js
@@ -8,6 +8,7 @@ var router = express.Router()
 
 var User = require('../models/User')
 var Category = require('../models/Category')
+var Content = require('../models/Content')
 
 router.use(function(req, res, next) {
   if (!req.userInfo.isAdmin) {
@@ -274,5 +275,184 @@ router.get('/category/delete', function(req, res) {
 
 })
 
+/**
+ * 内容首页
+ */
+router.get('/content', function (req, res) {
+  var page = Number(req.query.page || 1);
+  var limit = 10;
+  var pages = 0;
+
+  Content.count().then(function(count) {
+
+    //计算总页数
+    pages = Math.ceil(count / limit);
+    //取值不能超过pages
+    page = Math.min( page, pages );
+    //取值不能小于1
+    page = Math.max( page, 1 );
+
+    var skip = (page - 1) * limit;
+
+    /*
+    * 1: 升序
+    * -1: 降序
+    * */
+    Content.find().sort({_id: -1}).limit(limit).skip(skip).populate('category').then(function(contents) {
+      // console.log(contents)
+      res.render('admin/content_index', {
+        userInfo: req.userInfo,
+        contents: contents,
+
+        count: count,
+        pages: pages,
+        limit: limit,
+        page: page
+      });
+    });
+
+  });
+
+})
+
+/**
+ * 内容添加
+ */
+router.get('/content/add', function (req, res) {
+  Category.find().sort({_id: -1}).then(function (categories) {
+    res.render('admin/content_add', {
+      userInfo: req.userInfo,
+      categories: categories
+    })
+  })
+})
+
+/**
+ * 内容保存
+ */
+router.post('/content/add', function (req, res) {
+  if ( req.body.category == '' ) {
+    res.render('admin/error', {
+      userInfo: req.userInfo,
+      message: '内容分类不能为空'
+    })
+    return;
+  }
+
+  if ( req.body.title == '' ) {
+    res.render('admin/error', {
+      userInfo: req.userInfo,
+      message: '内容标题不能为空'
+    })
+    return;
+  }
+  //保存数据到数据库
+  new Content({
+    category: req.body.category,
+    title: req.body.title,
+    user: req.userInfo._id.toString(),
+    description: req.body.description,
+    content: req.body.content
+  }).save().then(function(rs) {
+    res.render('admin/success', {
+      userInfo: req.userInfo,
+      message: '内容保存成功',
+      url: '/admin/content'
+    })
+  });
+
+
+})
+
+/**
+ * 内容修改
+ */
+router.get('/content/edit', function(req, res) {
+
+  var id = req.query.id || '';
+
+  var categories = [];
+
+  Category.find().sort({_id: 1}).then(function(rs) {
+
+    categories = rs;
+
+    return Content.findOne({
+      _id: id
+    }).populate('category');
+  }).then(function(content) {
+
+    if (!content) {
+      res.render('admin/error', {
+        userInfo: req.userInfo,
+        message: '指定内容不存在'
+      });
+      return Promise.reject();
+    } else {
+      res.render('admin/content_edit', {
+        userInfo: req.userInfo,
+        categories: categories,
+        content: content
+      })
+    }
+  });
+
+});
+
+/**
+ * 保存修改内容
+ */
+router.post('/content/edit', function(req, res) {
+  var id = req.query.id || '';
+
+  if ( req.body.category == '' ) {
+    res.render('admin/error', {
+      userInfo: req.userInfo,
+      message: '内容分类不能为空'
+    })
+    return;
+  }
+
+  if ( req.body.title == '' ) {
+    res.render('admin/error', {
+      userInfo: req.userInfo,
+      message: '内容标题不能为空'
+    })
+    return;
+  }
+
+  Content.update({
+    _id: id
+  }, {
+    category: req.body.category,
+    title: req.body.title,
+    description: req.body.description,
+    content: req.body.content
+  }).then(function() {
+    res.render('admin/success', {
+      userInfo: req.userInfo,
+      message: '内容保存成功',
+      url: '/admin/content'
+    })
+  });
+
+});
+
+/**
+ * 内容删除
+ */
+router.get('/content/delete', function(req, res) {
+  var id = req.query.id || '';
+
+  Content.remove({
+    _id: id
+  }).then(function() {
+    res.render('admin/success', {
+      userInfo: req.userInfo,
+      message: '删除成功',
+      url: '/admin/content'
+    });
+  });
+});
 
 module.exports = router
