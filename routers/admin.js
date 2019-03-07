8eef6a3f8aab594475be1f1940842daafd146984
杨伟锋 <paranoidyang@163.com>
2019-03-07 11:38:38 +0800

    实现用户列表分页效果

--- routers/admin.js
+++ routers/admin.js
@@ -30,14 +30,46 @@ router.get('/', function(req, res, next) {
  * 用户管理
  */
 router.get('/user', function(req, res, next) {
-  /*从数据库中读取所有的用户数据*/
-  User.find().then(function (users) {
-    res.render('admin/user_index', {
-      userInfo: req.userInfo,
-      users: users
-    })
+  /*
+    * 从数据库中读取所有的用户数据
+    *
+    * limit(Number) : 限制获取的数据条数
+    *
+    * skip(2) : 忽略数据的条数
+    *
+    * 每页显示2条
+    * 1 : 1-2 skip:0 -> (当前页-1) * limit
+    * 2 : 3-4 skip:2
+    * */
 
-  })
+  var page = Number(req.query.page || 1);
+  var limit = 10;
+  var pages = 0;
+
+  User.count().then(function(count) {
+
+    //计算总页数
+    pages = Math.ceil(count / limit);
+    //取值不能超过pages
+    page = Math.min( page, pages );
+    //取值不能小于1
+    page = Math.max( page, 1 );
+    //跳过数据条数
+    var skip = (page - 1) * limit;
+
+    User.find().limit(limit).skip(skip).then(function(users) {
+      res.render('admin/user_index', {
+        userInfo: req.userInfo,
+        users: users,
+
+        count: count,
+        pages: pages,
+        limit: limit,
+        page: page
+      });
+    });
+
+  });
 
 
 });
