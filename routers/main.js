1c3a5e74785437945cba2daa7d0f5d4a45ae76f7
杨伟锋 <paranoidyang@163.com>
2019-03-08 16:28:15 +0800

    内容详情页的展示和阅读数的处理

--- routers/main.js
+++ routers/main.js
@@ -8,26 +8,37 @@ var router = express.Router()
 var Category = require('../models/Category')
 var Content = require('../models/Content')
 
-
-router.get('/', function (req, res, next) {
-
-  var data = {
+var data
+/*通用数据*/
+router.use(function (req, res, next) {
+  data = {
     userInfo: req.userInfo,
     categories: [],
+  }
+  Category.find().then(function (categories) {
+    data.categories = categories
+    next()
+  })
+})
 
-    count: 0,
-    page: Number(req.query.page || 1),
-    limit: 10,
-    pages: 0
+/**
+ * 首页
+ */
+router.get('/', function (req, res, next) {
+
+    data.category = req.query.category || '',
+    data.count = 0,
+    data.page = Number(req.query.page || 1),
+    data.limit = 4,
+    data.pages = 0
+
+  var where = {}
+  if(data.category) {
+    where.category = data.category
+
   }
 
-  /*读取所有的分类请求*/
-  Category.find().then(function (categories) {
-
-    data.categories = categories
-    return Content.count()
-
-  }).then(function (count) {
+  Content.where(where).count().then(function (count) {
 
     data.count = count
     //计算总页数
@@ -39,7 +50,7 @@ router.get('/', function (req, res, next) {
 
     var skip = (data.page - 1) * data.limit;
 
-    return Content.find().sort({_id: -1}).limit(data.limit).skip(skip).populate(['category', 'user']).sort({
+    return Content.where(where).find().sort({_id: -1}).limit(data.limit).skip(skip).populate(['category', 'user']).sort({
       addTime: -1
     })
 
@@ -49,6 +60,29 @@ router.get('/', function (req, res, next) {
     res.render('main/index', data)
   })
 
+
+})
+
+/**
+ * 详情页
+ */
+router.get('/detail', function (req, res){
+
+  var contentId = req.query.contentid || '';
+
+  Content.findOne({
+    _id: contentId
+  }).then(function (content) {
+
+    data.content = content;
+    console.log('文章信息', data.userInfo.username)
+
+    /*记录阅读数*/
+    content.views++;
+    content.save();
+
+    res.render('main/detail', data);
+  });
 
 })
 
