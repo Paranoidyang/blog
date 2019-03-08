339e976b9ead83f0d85a1a8ff57906c40e4a6c96
杨伟锋 <paranoidyang@163.com>
2019-03-08 10:55:29 +0800

    前台内容分页展示

--- routers/main.js
+++ routers/main.js
@@ -6,21 +6,48 @@ var express = require('express')
 var router = express.Router()
 
 var Category = require('../models/Category')
+var Content = require('../models/Content')
 
 
 router.get('/', function (req, res, next) {
 
+  var data = {
+    userInfo: req.userInfo,
+    categories: [],
+
+    count: 0,
+    page: Number(req.query.page || 1),
+    limit: 10,
+    pages: 0
+  }
+
   /*读取所有的分类请求*/
   Category.find().then(function (categories) {
-    console.log(categories)
-    res.render('main/index', {
-      userInfo: req.userInfo,
-      categories: categories
+
+    data.categories = categories
+    return Content.count()
+
+  }).then(function (count) {
+
+    data.count = count
+    //计算总页数
+    data.pages = Math.ceil(count / data.limit);
+    //取值不能超过data.pages
+    data.page = Math.min( data.page, data.pages );
+    //取值不能小于1
+    data.page = Math.max( data.page, 1 );
+
+    var skip = (data.page - 1) * data.limit;
+
+    return Content.find().sort({_id: -1}).limit(data.limit).skip(skip).populate(['category', 'user']).sort({
+      addTime: -1
     })
 
-
+  }).then(function (contents) {
+    data.contents = contents
+    console.log('data', data)
+    res.render('main/index', data)
   })
-
 
 
 })
