4c07290e17d61f2025116faf473b9d1e78fce7f9
杨伟锋 <paranoidyang@163.com>
2019-03-09 22:50:17 +0800

    内容评论的实现

--- routers/api.js
+++ routers/api.js
@@ -5,6 +5,7 @@
 var express = require('express')
 var router = express.Router()
 var User = require('../models/User')
+var Content = require('../models/Content')
 
 /*统一返回格式*/
 var responseData
@@ -116,6 +117,29 @@ router.post('/user/login', function(req, res) {
 router.get('/user/logout', function(req, res) {
   req.cookies.set('userInfo', null);
   res.json(responseData);
+})
+
+/*评论提交*/
+router.post('/comment/post', function(req, res) {
+  //内容的id
+  var contentId = req.body.contentid || '';
+  var postData = {
+    username: req.userInfo.username,
+    postTime: new Date(),
+    content: req.body.content
+  };
+
+  //查询当前这篇内容的信息
+  Content.findOne({
+    _id: contentId
+  }).then(function(content) {
+    content.comments.push(postData);
+    return content.save();
+  }).then(function(newContent) {
+    responseData.message = '评论成功';
+    responseData.data = newContent;
+    res.json(responseData);
+  });
 })
 
 module.exports = router
